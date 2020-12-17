18dd979f0f380d439cc1e137b3510dc5f4bdbb3b
jgofficial23 <73702065+jgofficial23@users.noreply.github.com>
2020-12-17 05:51:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function setDate(){
+ 
+   
+  document.getElementById('date').innerHTML = new Date().toDateString();
+  document.getElementById("time").innerHTML= new Date().toTimeString();
+  
+}
